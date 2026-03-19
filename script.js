6012894f20ce751ae723a2f460f0b509b06dd0de
darsh-844 <darshjaiswal2007@gmail.com>
2026-03-19 11:51:33 +0530

    FETCH

--- script.js
+++ script.js
@@ -288,9 +288,14 @@ document.write(" Loop Ended ");   */
 // });
 // promise.then(result => console.log(result)).catch(error => console.error(error));
 
-async function getData(){
-    let response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
-    let data = await response.json();
-    console.log(data);
-}
-getData();
+// async function getData(){
+//     let response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+//     let data = await response.json();
+//     console.log(data);
+// }
+// getData();
+
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error("Error fetching data: ", error));
